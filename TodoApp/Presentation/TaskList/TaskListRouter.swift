ab0f3ce6a871115c974f13b49baf07b988a8b8c4
syogo-user <s.photo.trumpet.005@gmail.com>
2024-01-21 01:07:54 +0900

    編集画面実装

--- TodoApp/Presentation/TaskList/TaskListRouter.swift
+++ TodoApp/Presentation/TaskList/TaskListRouter.swift
@@ -1,26 +1,26 @@
+////
+////  TaskListRouter.swift
+////  TodoApp
+////
+////  Created by 小野寺祥吾 on 2023/03/18.
+////
 //
-//  TaskListRouter.swift
-//  TodoApp
+//import Foundation
+//import UIKit
 //
-//  Created by 小野寺祥吾 on 2023/03/18.
+//extension TaskListViewController {
+//    /// 追加画面に遷移
+//    func toAddTask() {
+//        guard let addTaskVC = R.storyboard.list.addTaskVC() else { return }
+////        addTaskVC.delegate = self
+//        self.present(addTaskVC, animated: true)
+//    }
 //
-
-import Foundation
-import UIKit
-
-extension TaskListViewController {
-    /// 追加画面に遷移
-    func toAddTask() {
-        guard let addTaskVC = R.storyboard.list.addTaskVC() else { return }
-//        addTaskVC.delegate = self
-        self.present(addTaskVC, animated: true)
-    }
-
-    /// 更新画面に遷移
-    func toUpdateTask(taskInfoItem: TaskInfoItem) {
-        guard let updateTaskVC =  R.storyboard.list.updateTaskVC() else { return }
-        updateTaskVC.setUpdateTask(task: taskInfoItem)
-//        updateTaskVC.delegate = self
-        navigationController?.pushViewController(updateTaskVC, animated: true)
-    }
-}
+//    /// 更新画面に遷移
+//    func toUpdateTask(taskInfoItem: TaskInfoItem) {
+//        guard let updateTaskVC =  R.storyboard.list.updateTaskVC() else { return }
+//        updateTaskVC.setUpdateTask(task: taskInfoItem)
+////        updateTaskVC.delegate = self
+//        navigationController?.pushViewController(updateTaskVC, animated: true)
+//    }
+//}
